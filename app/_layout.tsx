afcbe91233156c57736c337bb55439afe73da65a
kwkimsd <76944401+kwkimsd@users.noreply.github.com>
2025-04-09 16:37:30 -0700

    Added new routes and layouts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -44,7 +44,8 @@ export default function RootLayout() {
       <StatusBar barStyle="default" />
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />\
+        <Stack.Screen name="(main)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ClerkProvider>
