11de3967486e60f6c2d5835f2e1f9995597540a1
kwkimsd <76944401+kwkimsd@users.noreply.github.com>
2025-04-10 15:36:37 -0700

    Added tab icons for tab bar.

--- app/(main)/(tabs)/_layout.tsx
+++ app/(main)/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import { icons } from "@/constants";
 import { Tabs } from "expo-router";
 import { Image, ImageSourcePropType, View } from "react-native";
 
@@ -8,19 +9,60 @@ const TabIcon = ({
   source: ImageSourcePropType;
   focused: boolean;
 }) => (
-  <View className={`flex justify-center items-center rounded-full`}>
+  <View className={`flex justify-center items-center rounded-full ${focused}`}>
     <View>
-      <Image source={source} />
+      <Image
+        source={source}
+        resizeMode="contain"
+        className="w-7 h-7"
+        tintColor="white"
+      />
     </View>
   </View>
 );
 
 const Layout = () => (
   <Tabs>
-    <Tabs.Screen name="home" />
-    <Tabs.Screen name="saved" />
-    <Tabs.Screen name="chat" />
-    <Tabs.Screen name="profile" />
+    <Tabs.Screen
+      name="home"
+      options={{
+        title: "Home",
+        headerShown: false,
+        tabBarIcon: ({ focused }: { focused: boolean }) => (
+          <TabIcon focused={focused} source={icons.home} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="saved"
+      options={{
+        title: "Saved",
+        headerShown: false,
+        tabBarIcon: ({ focused }: { focused: boolean }) => (
+          <TabIcon focused={focused} source={icons.bookmark} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="chat"
+      options={{
+        title: "Chat",
+        headerShown: false,
+        tabBarIcon: ({ focused }: { focused: boolean }) => (
+          <TabIcon focused={focused} source={icons.chat} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="profile"
+      options={{
+        title: "Profile",
+        headerShown: false,
+        tabBarIcon: ({ focused }: { focused: boolean }) => (
+          <TabIcon focused={focused} source={icons.profile} />
+        ),
+      }}
+    />
   </Tabs>
 );
 
