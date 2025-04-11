2c861a308880d5598d46a6b75a80182ea35487de
kwkimsd <76944401+kwkimsd@users.noreply.github.com>
2025-04-11 16:34:17 -0700

    Added a new search tab and worked on the home UI.

--- app/(main)/(tabs)/home.tsx
+++ app/(main)/(tabs)/home.tsx
@@ -1,11 +1,122 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import { icons } from "@/constants";
 
 const Home = () => {
+  const propertyTypes = [
+    "Apartments",
+    "Villas",
+    "Flats",
+    "Condominiums",
+    "Townhouses",
+  ];
+
+  const data = [
+    {
+      property_id: "12345",
+      address: "USA",
+      city: "Los Angeles",
+      state: "CA",
+      zip_code: "29292",
+      property_image: "https://picsum.photos/200/300?random=1",
+      property_type: "single_family",
+      square_footage: 3000,
+      bedrooms: 2,
+      bathrooms: 2,
+      estimated_price: 5000000,
+      year_built: 2023,
+    },
+    {
+      property_id: "23232",
+      address: "USA",
+      city: "Carlsbad",
+      state: "CA",
+      zip_code: "12451",
+      property_image: "https://loremflickr.com/200/200?random=1",
+      property_type: "condo",
+      square_footage: 2000,
+      bedrooms: 3,
+      bathrooms: 3,
+      estimated_price: 6500000,
+      year_built: 2022,
+    },
+    {
+      property_id: "63454",
+      address: "USA",
+      city: "Sacramento",
+      state: "CA",
+      zip_code: "76474",
+      property_image: "https://loremflickr.com/200/200?random=1",
+      property_type: "townhouse",
+      square_footage: 3100,
+      bedrooms: 3,
+      bathrooms: 2,
+      estimated_price: 3700000,
+      year_built: 2021,
+    },
+  ];
+
+  const [activePropertyType, setActivePropertyType] = useState("Apartments");
+
   return (
-    <View>
-      <Text> Home</Text>
-    </View>
+    <SafeAreaView className="flex">
+      <View className="flex flex-row justify-center items-center">
+        <Image
+          source={icons.leftArrow}
+          className="w-5 h-5"
+          resizeMode="contain"
+        />
+        <Text>Feed</Text>
+        <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
+      </View>
+      <View className="w-full p-5">
+        <Text>Explore Now</Text>
+        <FlatList
+          data={propertyTypes}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              className={`border rounded-full py-2 px-3 ${
+                activePropertyType === item ? "bg-[#b8c0d3]" : ""
+              }`}
+            >
+              <Text>{item}</Text>
+            </TouchableOpacity>
+          )}
+          contentContainerStyle={{ columnGap: 8 }}
+          horizontal
+        />
+      </View>
+      <View>
+        <View className="flex flex-row justify-between items-center">
+          <Text>Recommended Homes</Text>
+          <Text>See All</Text>
+        </View>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <View className="w-full">
+              <Image
+                source={{ uri: item.property_image }}
+                resizeMode="contain"
+                className="w-880 h-80"
+              />
+              <Text>{item.city}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item?.property_id}
+          contentContainerStyle={{ columnGap: 10 }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
