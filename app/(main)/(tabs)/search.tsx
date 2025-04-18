619a17eea4d04d18f062ece170f453cf6e51d472
kwkimsd <76944401+kwkimsd@users.noreply.github.com>
2025-04-18 16:31:33 -0700

    Set up search page and installed necessary packages.

--- app/(main)/(tabs)/search.tsx
+++ app/(main)/(tabs)/search.tsx
@@ -1,11 +1,31 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, TextInput, Button } from "react-native";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView>
+      <View className="p-10">
+        <Text>Search</Text>
+        <TextInput
+          placeholder="e.g. Homes in Miami under 800k"
+          value={search}
+          onChangeText={setSearch}
+          className="border p-3 mb-2 rounded-lg"
+        />
+        <Button title="Search" />
+      </View>
+      <View className="p-10">
+        <Text>
+          MAP DISPLAY MAP DISPLAY MAP DISPLAY MAP DISPLAY MAP DISPLAY MAP
+          DISPLAY
+        </Text>
+      </View>
+      <View>
+        <Text>BOTTOM TAB DISPLAY</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
