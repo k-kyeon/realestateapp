3d37fe86c1154c3747890181841b808c17abe823
k-kyeon <150563468+k-kyeon@users.noreply.github.com>
2025-04-22 15:21:45 -0700

    Added chat page layout.

--- app/(main)/(tabs)/chat.tsx
+++ app/(main)/(tabs)/chat.tsx
@@ -1,11 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, Image } from "react-native";
 import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import { images } from "@/constants";
 
 const Chat = () => {
   return (
-    <View>
-      <Text>Chat</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <ScrollView className="px-5">
+        <Text className="text-2xl font-MontserratBold my-3">Chat</Text>
+        <View className="flex justify-center items-center mt-20">
+          <Image
+            source={images.chatImage}
+            className="w-[300px] h-[300px]"
+            resizeMode="contain"
+          />
+          <Text className="text-2xl font-MontserratSemiBold mt-2">
+            No Messages Yet
+          </Text>
+          <Text className="text-xl font-MontserratMedium text-center mt-2">
+            Start a conversation with your agent today
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
