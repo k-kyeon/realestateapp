46c3c6009cf2e88d65cd09139f0deeaff9268a01
k-kyeon <150563468+k-kyeon@users.noreply.github.com>
2025-04-29 03:22:10 -0700

    Search page UI changed. Search bar hovers over Map display. Fixed issue with keyboard dismiss when clicking map.

--- components/GoogleAutoCompleteInput.tsx
+++ components/GoogleAutoCompleteInput.tsx
@@ -1,80 +1,73 @@
-import { Image, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { GoogleInputProps } from "@/types/type";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const googlePlacesApiKey = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
 
-const GoogleAutoCompleteInput = ({
-  icon,
-  iconStyles,
-  onLocationSelect,
-}: GoogleInputProps) => {
+const GoogleAutoCompleteInput = ({ onLocationSelect }: GoogleInputProps) => {
   return (
-    <View className="flex flex-row justify-center items-center mx-2">
-      <GooglePlacesAutocomplete
-        fetchDetails
-        placeholder="Search city (e.g. Miami)"
-        query={{
-          key: googlePlacesApiKey,
-          language: "en",
-        }}
-        debounce={200}
-        textInputProps={{
-          placeholderTextColor: "#999999",
-        }}
-        styles={{
-          textInput: {
-            backgroundColor: "white",
-            fontSize: 15,
-            fontWeight: "400",
-            width: "100%",
-            borderRadius: 15,
-          },
-          textInputContainer: {
-            justifyContent: "center",
-            alignItems: "center",
-            borderRadius: 20,
-            marginRight: 10,
-            position: "relative",
-            shadowColor: "#dbdbdb",
-          },
-          listView: {
-            backgroundColor: "white",
-            position: "relative",
-            top: 0,
-            width: "100%",
-            borderRadius: 12,
-            shadowColor: "#dbdbdb",
-            zIndex: 99,
-          },
-        }}
-        onPress={(data, details = null) => {
-          const lat = details?.geometry.location.lat!;
-          const lng = details?.geometry.location.lng!;
+    <GooglePlacesAutocomplete
+      fetchDetails
+      placeholder="Search city (e.g. Miami)"
+      query={{
+        key: googlePlacesApiKey,
+        language: "en",
+      }}
+      debounce={200}
+      textInputProps={{
+        placeholderTextColor: "#999999",
+        style: {
+          backgroundColor: "white",
+          fontSize: 15,
+          fontWeight: "400",
+          width: "100%",
+          borderRadius: 15,
+          height: 45,
+          paddingVertical: 10,
+          paddingHorizontal: 10,
+        },
+      }}
+      styles={{
+        textInputContainer: {
+          borderRadius: 20,
+          backgroundColor: "white",
+          marginRight: 10,
+          position: "relative",
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.2,
+          shadowRadius: 4,
+          elevation: 4,
+          paddingHorizontal: 10,
+        },
+        listView: {
+          backgroundColor: "white",
+          width: "100%",
+          borderRadius: 12,
+          zIndex: 99,
+          elevation: 2,
+          position: "absolute",
+          top: 50,
+        },
+      }}
+      onPress={(data, details = null) => {
+        const lat = details?.geometry.location.lat!;
+        const lng = details?.geometry.location.lng!;
 
-          const cityComponent = details?.address_components.find((component) =>
-            component.types.includes("locality"),
-          );
-          const city =
-            cityComponent?.long_name || data.structured_formatting.main_text;
+        const cityComponent = details?.address_components.find((component) =>
+          component.types.includes("locality"),
+        );
+        const city =
+          cityComponent?.long_name || data.structured_formatting.main_text;
 
-          const stateComponent = details?.address_components.find((component) =>
-            component.types.includes("administrative_area_level_1"),
-          );
-          const state = stateComponent?.short_name;
+        const stateComponent = details?.address_components.find((component) =>
+          component.types.includes("administrative_area_level_1"),
+        );
+        const state = stateComponent?.short_name;
 
-          onLocationSelect({ lat, lng, city, state });
-        }}
-      />
-      {icon && (
-        <TouchableOpacity>
-          <View className={`border rounded-full p-3 ${iconStyles}`}>
-            <Image source={icon} className="w-5 h-5" resizeMode="contain" />
-          </View>
-        </TouchableOpacity>
-      )}
-    </View>
+        onLocationSelect({ lat, lng, city, state });
+      }}
+    />
   );
 };
 
